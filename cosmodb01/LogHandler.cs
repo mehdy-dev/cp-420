d48ee5e40d1a3666b1e7d12615f932b297dddd7f
Mehdi Alipour Babaee <mehdi.alipour.babaee@accenture.com>
2024-12-26 23:13:00 +0100

    cosmo db custom logger

---
+++ cosmodb01/LogHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cosmodb01
+{
+    public class LogHandler : RequestHandler
+    {
+        public override async Task<ResponseMessage> SendAsync(RequestMessage request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"[{request.Method.Method}]\t{request.RequestUri}");
+
+            ResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            Console.WriteLine($"[{Convert.ToInt32(response.StatusCode)}]\t{response.StatusCode}");
+
+            return response;
+        }
+    }
+}
---
+++ cosmodb01/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos.Fluent;
+using System;
+using Azure.Identity;
+using Azure.Core;
+using Microsoft.Azure.Cosmos;
+using System.Net;
+
+namespace cosmodb01
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            CosmosClientBuilder builder = new CosmosClientBuilder("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+                .WithConsistencyLevel(ConsistencyLevel.Eventual);
+
+            builder.AddCustomHandlers(new LogHandler());
+
+            CosmosClient client = builder.Build();
+
+            Database result = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+
+        }
+    }
+}
